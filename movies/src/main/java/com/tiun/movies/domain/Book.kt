bad92faba46fdd4a31ae92bf5dcfa7ddc990886f
samurenko roma <samurenkoroma@gmail.com>
2023-01-01 17:02:17 +0200

    to be

---
+++ movies/src/main/java/com/tiun/movies/domain/Book.kt
@@ -0,0 +1,6 @@
+package com.tiun.movies.domain
+
+data class Book(
+    private val id: String,
+    private val title: String
+)
---
+++ movies/src/main/java/com/tiun/movies/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tiun.movies.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tiun.movies.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
