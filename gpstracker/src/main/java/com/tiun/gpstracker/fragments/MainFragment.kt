78927aedb581d4d246c391b3aa821b184f056f67
samurenko roma <samurenkoroma@gmail.com>
2023-01-07 18:45:51 +0200

    add osm maps

--- gpstracker/src/main/java/com/tiun/gpstracker/fragments/MainFragment.kt
+++ gpstracker/src/main/java/com/tiun/gpstracker/fragments/MainFragment.kt
@@ -1,11 +1,15 @@
 package com.tiun.gpstracker.fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import com.tiun.gpstracker.databinding.FragmentMainBinding
+import org.osmdroid.config.Configuration
+import org.osmdroid.library.BuildConfig
 
 class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
@@ -14,8 +18,17 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        settingsOSM()
         binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    private fun settingsOSM() {
+        Configuration.getInstance().load(
+            activity as AppCompatActivity,
+            activity?.getSharedPreferences("osm_pref", Context.MODE_PRIVATE)
+        )
+        Configuration.getInstance().userAgentValue = BuildConfig.APPLICATION_ID
     }
 
     companion object {
