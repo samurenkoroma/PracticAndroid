379321978a21962e9fdbf29e1179cbc2f6555470
samurenko roma <samurenkoroma@gmail.com>
2023-01-08 21:20:12 +0200

    changed main layout

--- gpstracker/src/main/java/com/tiun/gpstracker/fragments/MainFragment.kt
+++ gpstracker/src/main/java/com/tiun/gpstracker/fragments/MainFragment.kt
@@ -42,6 +42,10 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         registerPermissions()
+    }
+
+    override fun onResume() {
+        super.onResume()
         checkLocPermission()
     }
 
